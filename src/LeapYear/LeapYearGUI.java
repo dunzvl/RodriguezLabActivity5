827b4c38262c55de98fc56d35cb8d58ef703395b
Blu Verdonz <cuesoblube@gmail.com>
2022-12-16 22:11:05 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,13 +1,40 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+    private JLabel txtInputYear;
 
     public static void main(String[] args){
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.panel1);
+        app.setTitle("Leap Year Checker");
+        app.setSize(400, 300);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 
+    public LeapYearGUI() {
+        txtInputYear.setText("Input Year");
+        btnCheckYear.setText("Check");
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());
+                if(isLeapYear(year)){
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                }
+            }
+        });
+    }
+
+    private boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
