ea8e8e99e1e67f6d9055e2181a4aac52583830ab
Blu Verdonz <cuesoblube@gmail.com>
2022-12-17 01:25:51 +0800

    Add FoodOrdering main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,10 +2,14 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza, cBurger, cFries, cSoftDrinks, cTea, cSundae;
     private JRadioButton rbNone, rb5, rb10, rb15;
     private JButton btnOrder;
     private JLabel lblFoods, lblDiscount;
+
+    public static void main(String[] args){
+
+    }
 }
